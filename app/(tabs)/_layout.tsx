81141cdeeeef6aed12f5f31f0bb80ac5844e7623
Grian Gajila <griangajila2@gmail.com>
2024-12-09 15:39:43 +0800

    chore: update structure

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const TabLayout = () => {
+	return (
+		<View>
+			<Text>TabLayout</Text>
+		</View>
+	);
+};
+
+export default TabLayout;
