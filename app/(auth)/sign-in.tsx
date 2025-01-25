313029b39ca6d5b083089fe004c825e078750531
Grian Gajila <griangajila2@gmail.com>
2025-01-25 23:28:57 +0800

    chore: login updated

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,15 +1,20 @@
 import Sugarcane from "@/assets/logo/sugarcane.png";
 import components from "@/components";
 import logo from "@/constant/logo";
+import axios from "axios";
 import { Redirect } from "expo-router";
 import React, { useEffect, useState } from "react";
 import { Dimensions, Image, ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+
 const SignIn = () => {
 	const [username, setUsername] = useState("");
 	const [password, setPassword] = useState("");
 	const [loading, setLoading] = useState(false);
 	const [disabled, setDisabled] = useState(false);
+	const [error, setError] = useState(false);
+	const [success, setSuccess] = useState(false);
+	const [requestError, setRequestError] = useState(false);
 
 	const handleChangeUsername = (eventValue: any) => {
 		setUsername(eventValue);
@@ -22,50 +27,53 @@ const SignIn = () => {
 	useEffect(() => {
 		if (username && password) {
 			setDisabled(false);
+			setError(false);
+			setRequestError(false);
 		} else {
 			setDisabled(true);
 		}
 	}, [username, password]);
 
-	const handleLogin = () => {
+	if (success) {
+		return <Redirect href="/home" />;
+	}
+
+	const handleLogin = async () => {
 		setLoading(true);
-		if (username && password) {
-			const data = {
-				username,
-				password,
-			};
-			if (data.username === "admin" && data.password === "admin") {
-				setUsername("");
-				setPassword("");
-				setLoading(false);
-				return <Redirect href="/home" />;
-			} else {
-				console.log("Invalid username or password");
-			}
+		const data = {
+			username,
+			password,
+		};
+		try {
+			axios.post("http://localhost:3000/auth/login", data).then((res) => {
+				if (res.data.status === "ok") {
+					console.log({ message: "Login successful", data: res.data });
+					setUsername("");
+					setPassword("");
+					setLoading(false);
+					setDisabled(true);
+					setSuccess(true);
+				} else {
+					setLoading(false);
+					setDisabled(true);
+					setUsername("");
+					setPassword("");
+					setError(true);
+					setSuccess(false);
+					console.log({ message: "Login failed", data: res.data });
+				}
+			});
+		} catch (error) {
+			setLoading(false);
+			setDisabled(true);
+			setUsername("");
+			setPassword("");
+			setError(true);
+			setSuccess(false);
+			setRequestError(true);
+			console.log({ message: "Login failed", data: error });
 		}
 	};
-
-	// const handleLogin = async () => {
-	// 	if (username && password) {
-	// 		const data = {
-	// 			username,
-	// 			password,
-	// 		};
-	// 		try {
-	// 			const response = await fetch("http://localhost:3000/api/auth/login", {
-	// 				method: "POST",
-	// 				headers: {
-	// 					"Content-Type": "application/json",
-	// 				},
-	// 				body: JSON.stringify(data),
-	// 			});
-	// 			const responseData = await response.json();
-	// 			console.log("This is responseData", responseData);
-	// 		} catch (error) {
-	// 			console.log("This is error", error);
-	// 		}
-	// 	}
-	// };
 
 	return (
 		<SafeAreaView className="bg-primary h-full">
@@ -77,6 +85,20 @@ const SignIn = () => {
 					}}
 				>
 					<Image className="w-full" resizeMode="contain" source={Sugarcane} />
+					<View
+						className={` bg-white rounded-2xl p-3 text-center w-full justify-center items-center ${
+							error ? "absolute" : "hidden"
+						}`}
+					>
+						<Text className="text-red-700 text-lg font-bold">
+							{requestError ? "Request Error!" : "Login Error!"}
+						</Text>
+						<Text className="text-red-500 text-base font-bold">
+							{requestError
+								? "Please check your internet connection"
+								: "Invalid username or password"}
+						</Text>
+					</View>
 					<View className="w-full border flex-col blur-md items-center backdrop-blur-sm  pb-8 border-gray-300 bg-backdrop rounded-3xl -mt-12">
 						<Image source={logo.EsugarLogo} resizeMode="contain" />
 						<Text className="-mt-5 mb-10 text-xl text-white">
