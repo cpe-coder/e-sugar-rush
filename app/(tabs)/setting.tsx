f4c1319cfd98c0232e39537a76bd27a051d9299c
Grian Gajila <griangajila2@gmail.com>
2025-01-27 23:59:31 +0800

    chore: update settings

--- app/(tabs)/setting.tsx
+++ app/(tabs)/setting.tsx
@@ -1,13 +1,54 @@
+import icons from "@/constant/icons";
 import React from "react";
-import { ScrollView, Text, View } from "react-native";
+import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Setting = () => {
 	return (
-		<SafeAreaView className="h-full bg-primary">
+		<SafeAreaView className="h-full bg-primary py-8 px-6">
 			<ScrollView>
-				<View>
-					<Text>Setting</Text>
+				<View className="justify-center items-start">
+					<Text className="text-start mb-4 text-white font-medium text-xl">
+						Account
+					</Text>
+					<View className=" w-full justify-center items-center bg-primary3 border gap-5 border-gray-300 rounded-3xl py-8 px-10">
+						<View className="flex-row w-full justify-start items-center gap-10">
+							<Image
+								className="w-8 h-8"
+								tintColor="#fff"
+								resizeMode="contain"
+								source={icons.User}
+							/>
+							<Text className="text-xl font-medium text-white">
+								Edit Profile
+							</Text>
+						</View>
+						<View className="flex-row w-full justify-start items-center gap-10">
+							<Image
+								className="w-8 h-8"
+								tintColor="#fff"
+								resizeMode="contain"
+								source={icons.Security}
+							/>
+							<Text className="text-xl font-medium text-white">
+								Change Credentials
+							</Text>
+						</View>
+					</View>
+					<Text className="text-start mt-10 mb-4 text-white font-medium text-xl">
+						Action
+					</Text>
+					<View className="flex-row w-full justify-start items-center gap-10 bg-primary3 border border-gray-300 rounded-3xl py-8 px-10">
+						<Image
+							className="w-8 h-8"
+							tintColor="#fff"
+							resizeMode="contain"
+							source={icons.Exit}
+						/>
+						<TouchableOpacity>
+							<Text className="text-xl font-medium text-white">Log out</Text>
+						</TouchableOpacity>
+					</View>
 				</View>
 			</ScrollView>
 		</SafeAreaView>
--- constant/icons.ts
+++ constant/icons.ts
@@ -4,12 +4,15 @@ import Boy from "@/assets/icons/boy.png";
 import ClosedEye from "@/assets/icons/closedeye.png";
 import Control from "@/assets/icons/control.png";
 import Dry from "@/assets/icons/dry.png";
+import Exit from "@/assets/icons/exit.png";
 import Eye from "@/assets/icons/eye.png";
 import Home from "@/assets/icons/home.png";
 import Power from "@/assets/icons/power.png";
 import Roller from "@/assets/icons/roller.png";
+import Security from "@/assets/icons/security.png";
 import Setting from "@/assets/icons/setting.png";
 import Stat from "@/assets/icons/stat.png";
+import User from "@/assets/icons/user.png";
 
 const icons = {
 	Eye,
@@ -24,6 +27,9 @@ const icons = {
 	Dry,
 	Boil,
 	Roller,
+	Exit,
+	User,
+	Security,
 };
 
 export default icons;
