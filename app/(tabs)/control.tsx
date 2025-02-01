a6ce79e77862860b006803db6afe952e8ac0e020
Grian Gajila <griangajila2@gmail.com>
2025-02-01 15:55:59 +0800

    update controls

--- app/(tabs)/control.tsx
+++ app/(tabs)/control.tsx
@@ -1,7 +1,7 @@
 import components from "@/components";
 import icons from "@/constant/icons";
 import logo from "@/constant/logo";
-import { ref, set } from "firebase/database";
+import { get, onValue, ref, set } from "firebase/database";
 import React from "react";
 import {
 	Image,
@@ -18,6 +18,50 @@ const Control = () => {
 	const [refreshing, setRefreshing] = React.useState(false);
 	const [visibile, setVisible] = React.useState(false);
 	const [power, setPower] = React.useState(false);
+	const [isPower, setIsPower] = React.useState(false);
+	const [extract, setExtract] = React.useState(false);
+	const [isExtract, setIsExtract] = React.useState(false);
+	const [boil, setBoil] = React.useState(false);
+	const [isBoil, setIsBoil] = React.useState(false);
+	const [dry, setDry] = React.useState(false);
+	const [isDry, setIsDry] = React.useState(false);
+
+	React.useEffect(() => {
+		getPowerValue();
+		getExtractValue();
+		getBoilValue();
+		getDryValue();
+	});
+
+	const getPowerValue = async () => {
+		const valueRef = ref(database, "Controls/power");
+		const value = await get(valueRef);
+		setIsPower(value.val());
+	};
+	const getExtractValue = () => {
+		const valueRef = ref(database, "Controls/extract");
+		const subscribe = onValue(valueRef, (snapshot) => {
+			const value = snapshot.val();
+			setExtract(value);
+		});
+		return () => subscribe();
+	};
+	const getBoilValue = () => {
+		const valueRef = ref(database, "Controls/boil");
+		const subscribe = onValue(valueRef, (snapshot) => {
+			const value = snapshot.val();
+			setBoil(value);
+		});
+		return () => subscribe();
+	};
+	const getDryValue = () => {
+		const valueRef = ref(database, "Controls/dry");
+		const subscribe = onValue(valueRef, (snapshot) => {
+			const value = snapshot.val();
+			setDry(value);
+		});
+		return () => subscribe();
+	};
 
 	const onRefresh = React.useCallback(() => {
 		setRefreshing(true);
@@ -34,7 +78,28 @@ const Control = () => {
 		const valueRef = ref(database, "Controls/power");
 		await set(valueRef, power ? true : false);
 		setPower((prev) => !prev);
-		console.log("This is power", power);
+		setIsPower(power);
+	};
+
+	const activeExtract = async () => {
+		const valueRef = ref(database, "Controls/extract");
+		await set(valueRef, extract ? true : false);
+		setExtract((prev) => !prev);
+		setIsExtract(extract);
+	};
+
+	const activeBoil = async () => {
+		const valueRef = ref(database, "Controls/boil");
+		await set(valueRef, boil ? true : false);
+		setBoil((prev) => !prev);
+		setIsBoil(boil);
+	};
+
+	const activeDry = async () => {
+		const valueRef = ref(database, "Controls/dry");
+		await set(valueRef, dry ? true : false);
+		setDry((prev) => !prev);
+		setIsDry(dry);
 	};
 
 	return (
@@ -46,16 +111,39 @@ const Control = () => {
 			>
 				<View className="justify-center items-center">
 					<Image source={logo.EsugarLogo} />
+
 					<View className="border border-gray-300 bg-primary3 rounded-3xl py-6 mt-10 px-8 w-full">
+						<View className="absolute left-4  -top-5 flex-row justify-start items-center gap-1">
+							<View
+								className={`w-4 h-4 mr-2 border-[1px] border-gray-300 rounded-full ${
+									power ? "bg-black" : "bg-red-500"
+								}`}
+							></View>
+							<View
+								className={`w-4 h-4 border-[1px] border-gray-300 rounded-full ${
+									extract ? "bg-black" : "bg-yellow"
+								}`}
+							></View>
+							<View
+								className={`w-4 h-4 border-[1px] border-gray-300 rounded-full ${
+									boil ? "bg-black" : "bg-yellow"
+								}`}
+							></View>
+							<View
+								className={`w-4 h-4 border-[1px] border-gray-300 rounded-full ${
+									dry ? "bg-black" : "bg-yellow"
+								}`}
+							></View>
+						</View>
 						<Text className="text-white text-lg text-center mb-4">
 							MAIN CONTROLS
 						</Text>
 						<TouchableOpacity
 							onPress={activePower}
 							disabled={false}
-							className="bg-white flex-row gap-2 items-center justify-center py-2 w-full rounded-2xl"
+							className="flex-row gap-2 items-center justify-center py-2 w-full rounded-2xl bg-white "
 						>
-							<Text className="text-textColor text-2xl font-bold">Power</Text>
+							<Text className="text-2xl font-bold text-textColor ">Power</Text>
 							<Image
 								className="w-6 h-6"
 								tintColor="#024f8e"
@@ -64,7 +152,10 @@ const Control = () => {
 							/>
 						</TouchableOpacity>
 						<View className="flex-row items-center mt-2 justify-between">
-							<TouchableOpacity className="rounded-2xl bg-gray  w-24 gap-1 bg-other py-2 px-4 justify-center items-center">
+							<TouchableOpacity
+								onPress={activeExtract}
+								className="rounded-2xl  w-24 gap-1 py-2 px-4 justify-center items-center bg-primary"
+							>
 								<Image
 									className="w-8 h-8"
 									resizeMode="contain"
@@ -73,7 +164,10 @@ const Control = () => {
 								/>
 								<Text className="text-white">Extract</Text>
 							</TouchableOpacity>
-							<TouchableOpacity className="rounded-2xl w-24  gap-1 bg-other py-2 px-4 justify-center items-center">
+							<TouchableOpacity
+								onPress={activeBoil}
+								className="rounded-2xl  w-24 gap-1 py-2 px-4 justify-center items-center bg-primary"
+							>
 								<Image
 									className="w-8 h-8"
 									resizeMode="contain"
@@ -82,7 +176,10 @@ const Control = () => {
 								/>
 								<Text className="text-white">Boil</Text>
 							</TouchableOpacity>
-							<TouchableOpacity className="rounded-2xl w-24  gap-1 bg-other py-2 px-4 justify-center items-center">
+							<TouchableOpacity
+								onPress={activeDry}
+								className="rounded-2xl  w-24 gap-1 py-2 px-4 justify-center items-center bg-primary"
+							>
 								<Image
 									className="w-8 h-8"
 									resizeMode="contain"
